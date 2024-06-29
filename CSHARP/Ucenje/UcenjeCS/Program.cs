79e828e55913b291bf27c05b721c4e56b2c2a99d
DominikPuljic <1992pulja@gmail.com>
2024-06-29 14:53:13 +0200

    CSHARP

---
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+using UcenjeCS;
+
+E04UvjetnoGrananjeSwitch.Izvedi();
+
+//E03Z1.Izvedi();
+
+//E03UvjetnoGrananjeIf.Izvedi();
+
+//E02VarijableTipoviPodataka.Izvedi();
+
+//E01UlazIzlaz.Izvedi();
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, Edunova!");
+//Console.WriteLine("Osijek");
+
+// Ovo je komentar jedna linija
+
+/* 
+ 
+ Komentar kroz više linija
+ 
+ */
+
+
+//Console.WriteLine("Zadnja linija čšćđžČŠĆĐŽ");
