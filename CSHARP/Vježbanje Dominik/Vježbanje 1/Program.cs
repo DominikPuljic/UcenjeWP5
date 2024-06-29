d2fa11a269f702d33a38a06edb67360f203e2a6b
DominikPuljic <1992pulja@gmail.com>
2024-06-29 17:07:03 +0200

    Vježbe

---
+++ CSHARP/Vježbanje Dominik/Vježbanje 1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+
+
+
+
+
+
+using Vježbanje_1;
+
+VZ01.Izvedi();
+
+
+
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
---
+++ CSHARP/Vježbanje Dominik/Vježbanje 1/VZ01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vježbanje_1
+{
+    internal class VZ01
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine("bfsgdkgnsk");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
