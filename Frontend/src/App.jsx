aa0dc19214c36241bc39479db4edeb3fac871ff3
DominikPuljic <1992pulja@gmail.com>
2024-10-03 20:34:10 +0200

    Frontend produkcija

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container';
 import './App.css'
+import NavBarEdunova from './components/NavBarEdunova';
+import { Route, Routes } from 'react-router-dom';
+import { RouteNames } from './constants';
+import Pocetna from './pages/Pocetna';
+import SmjeroviPregled from './pages/smjerovi/SmjeroviPregled';
+import SmjeroviDodaj from './pages/smjerovi/SmjeroviDodaj';
+import SmjeroviPromjena from './pages/smjerovi/SmjeroviPromjena';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Container>
+      <NavBarEdunova />
+      <Routes>
+        <Route path={RouteNames.HOME} element={<Pocetna/>} />
+
+        <Route path={RouteNames.SMJER_PREGLED} element={<SmjeroviPregled/>}/>
+        <Route path={RouteNames.SMJER_NOVI} element={<SmjeroviDodaj/>}/>
+        <Route path={RouteNames.SMJER_PROMJENA} element={<SmjeroviPromjena/>}/>
+
+      </Routes>
+      <hr/>
+      &copy; Edunova
+    </Container>
+    
     </>
   )
 }
